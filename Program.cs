32084a24e56d517aace1e24702752fb05df2b1dc
91Edward <t.shaddow@yahoo.com>
2026-03-13 19:26:22 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<Truck> fleet = new List<Truck>();
+
+    static void Main()
+    {
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\n=== Fleet Maintenance Tracker ===");
+            Console.WriteLine("1. Add Truck");
+            Console.WriteLine("2. View Fleet");
+            Console.WriteLine("3. Exit");
+
+            Console.Write("Select option: ");
+            string choice = Console.ReadLine();
+            
+            switch (choice)
+            {
+                case "1":
+                    AddTruck();
+                    break;
+                case "2":
+                    ViewFleet();
+                    break;
+                case "3":
+                    running = false;
+                    break;
+            }
+        }
+    }
+
+    static void AddTruck()
+    {
+        Truck truck = new Truck();
+        Console.Write("Truck Model: ");
+        truck.Model = Console.ReadLine();
+
+        Console.Write("Resigstration: ");
+        truck.Registration = Console.ReadLine();
+
+        Console.Write("Last Service Date (yyyy- mm-dd): ");
+        truck.LastServiceDate = DateTime.Parse(Console.ReadLine());
+
+        fleet.Add(truck);
+
+        Console.WriteLine("Truck added successfully");
+    }
+
+    static void ViewFleet()
+    {
+        Console.WriteLine("\nFleet List:");
+
+        foreach(Truck truck in fleet)
+        {
+            truck.DisplayInfo();
+        }
+    }
+}
---
+++ Truck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Truck
+{
+    public string Model { get; set; }
+    public string Registration { get; set; }
+    public DateTime LastServiceDate { get; set; }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"{Model} | {Registration} | Last Service: {LastServiceDate.ToShortDateString()}");
+    }
+}
